43a4effaf0976ab490bd34c36330f5ae2c639b0d
Damien van den IJssel <dovandenijssel@live.nl>
2024-11-14 12:25:48 +0100

    made views and controllers for todo app

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::resource('todo', TodoController::class)
+    ->only(['index', 'show', 'store', 'update', 'destroy'])
+    ->middleware(['auth', 'verified']);
+
 require __DIR__.'/auth.php';
