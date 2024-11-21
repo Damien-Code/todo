1540964f4c87e9c05f9091488618f864744c3bec
Damien van den IJssel <dovandenijssel@live.nl>
2024-11-21 16:03:40 +0100

    styling for show page

--- resources/views/todo/show.blade.php
+++ resources/views/todo/show.blade.php
@@ -1,22 +1,31 @@
 @extends('layouts.default')
 @section('content')
-<div class="h-screen flex justify-center items-center flex-col">
-    <div class="h-24 w-80 flex justify-center items-center text-center">
-        <p class="text-white text-4xl">{{$todo->id}}</p>
+    <div class="h-screen flex justify-center items-center flex-col">
+        <div class="bg-gray-700 rounded-xl flex justify-center flex-col items-center text-center p-8">
+            <div class="h-24 w-80">
+                <p class="text-white text-4xl">{{$todo->id}}</p>
+            </div>
+            <div class="h-24 w-80">
+                <p class="text-white text-4xl">{{$todo->title}}</p>
+            </div>
+            <div class="h-24 w-80">
+                <p class="text-white text-4xl">{{$todo->description}}</p>
+            </div>
+            <div class="h-24 w-80">
+                <p class="text-white text-4xl">{{$todo->completed}}</p>
+            </div>
+            <div class="h-24 w-80">
+                <p class="text-white text-4xl">{{$todo->created_at}}</p>
+            </div>
+            <a href="{{route('todo.index')}}">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-full mb-5 m-16">Back
+                    <-
+                </button>
+            </a>
+            <a href="{{route('todo.edit', $todo->id)}}"
+               class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-full">
+                <button>Edit</button>
+            </a>
+        </div>
     </div>
-    <div class="h-24 w-80 flex justify-center items-center text-center">
-        <p class="text-white text-4xl">{{$todo->title}}</p>
-    </div>
-    <div class="h-24 w-80 flex justify-center items-center text-center">
-        <p class="text-white text-4xl">{{$todo->description}}</p>
-    </div>
-    <div class="h-24 w-80 flex justify-center items-center text-center">
-        <p class="text-white text-4xl">{{$todo->completed}}</p>
-    </div>
-    <div class="h-24 w-80 flex justify-center items-center text-center">
-        <p class="text-white text-4xl">{{$todo->created_at}}</p>
-    </div>
-    <a href="{{route('todo.index')}}"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-full mb-5 m-16" >Back <-</button></a>
-    <a href="{{route('todo.edit', $todo->id)}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-full"><button>Edit</button></a>
-</div>
 @endsection
